e47543b1d23d7a1c2aacfee69e19e9fb8ac561de
Devin Gray <devin.gray92@gmail.com>
2020-12-31 10:14:47 -0800

    removed proces.env from netlify build

--- src/config/ghostapi.ts
+++ src/config/ghostapi.ts
@@ -1,8 +1,8 @@
 import GhostContentAPI from '@tryghost/content-api';
 
 const ghost = new GhostContentAPI({
-  url: process.env.GHOST_BASE_URL || 'https://devingray.io',
-  key: process.env.GHOST_API_KEY || '07a56a659476f0a22f7c3b3ed4',
+  url:  'https://devingray.io',
+  key: '07a56a659476f0a22f7c3b3ed4',
   version: 'v3'
 });
 
