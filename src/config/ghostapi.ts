2dbaec3d8c41d7c3d5d6f70f727d88cb45ccdde1
Devin Gray <admin@devingray.io>
2023-06-02 05:48:01 -0700

    removed unnecessary dotenv import

--- src/config/ghostapi.ts
+++ src/config/ghostapi.ts
@@ -1,7 +1,4 @@
 import GhostContentAPI from '@tryghost/content-api';
-import dotenv from 'dotenv';
-
-dotenv.config();
 const ghost = new GhostContentAPI({
   url:  String(process.env.GHOST_URL),
   key: String(process.env.GHOST_KEY),
