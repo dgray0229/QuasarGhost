99571e940dcfe5558cfb42d08dd65b5d2624c184
Devin Gray <admin@devingray.io>
2023-06-07 19:06:06 -0700

    added ghost process.env vars to project

--- src/config/ghostapi.ts
+++ src/config/ghostapi.ts
@@ -1,8 +1,8 @@
-import GhostContentAPI from '@tryghost/content-api';
+import GhostContentAPI, { GhostContentAPIOptions } from '@tryghost/content-api';
 const ghost = new GhostContentAPI({
-  url:  'http://blogapi.devingray.io',
-  key: '845fa738b73938460645e6639e',
-  version: 'v5.0'
+  url:  String(process.env.GHOST_BASE_URL),
+  key: String(process.env.GHOST_API_KEY),
+  version: String(process.env.GHOST_API_VERSION) as GhostContentAPIOptions['version']
 });
 
 export default ghost;
