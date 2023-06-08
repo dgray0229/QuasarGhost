bf7f83bccdc8e22d6906966bc20ea8d778700fe9
Devin Gray <admin@devingray.io>
2023-06-07 18:57:01 -0700

    fix issue with dotenv

--- quasar.config.js
+++ quasar.config.js
@@ -9,6 +9,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable @typescript-eslint/no-floating-promises */
+require('dotenv').config();
 const { configure } = require('quasar/wrappers');
 
 module.exports = configure(function (ctx) {
@@ -50,7 +51,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      env: require('dotenv').config().parsed,
+      env: {...process.env}},
       vueRouterMode: 'history', // available values: 'hash', 'history'
 
       // transpile: false,
