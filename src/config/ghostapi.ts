f7bd0907a4037a93a6b4f619c10b15ba7fabb127
Devin Gray <admin@devingray.io>
2023-06-07 21:41:17 -0700

    Added typings to process.env

--- src/config/ghostapi.ts
+++ src/config/ghostapi.ts
@@ -1,4 +1,12 @@
 import GhostContentAPI from '@tryghost/content-api';
+
+declare const process: {
+  env: {
+    GHOST_API_URL: string;
+    GHOST_CONTENT_API_KEY: string;
+  };
+};
+
 const ghost = new GhostContentAPI({
   url:  String(process.env.GHOST_API_URL),
   key: String(process.env.GHOST_CONTENT_API_KEY),
